241264d6b9dced604cd993bfbaec2a0ba661c42e
Ethan Nixon <ethan.nixon62@gmail.com>
2020-09-26 16:29:24 -0700

    Added required dependencies

--- BadSyringe.cs
+++ BadSyringe.cs
@@ -1,11 +1,13 @@
 ﻿using BepInEx;
 using RoR2;
+using R2API.Utils;
+using R2API;
 
 namespace PurpleLeprecaun
 {
+    [R2APISubmoduleDependency(nameof(ItemAPI), nameof(ItemDropAPI))]
     [BepInDependency("com.bepis.r2api")]
-    //Change these
-    [BepInPlugin("com.ThatsSickLOL.BadSyringe", "Bad Syringe and if we see this exact name on Thunderstore we will deprecate your mod", "1.0.0")]
+    [BepInPlugin("com.ThatsSickLOL.BadSyringe", "Bad Syringe", "1.0.0")]
     public class BadSyringe : BaseUnityPlugin
     {
         public void Awake()
