f35b39894876be286b823fefefe3afe2ee3c4cdc
Ethan Nixon <ethan.nixon62@gmail.com>
2020-09-26 16:04:31 -0700

    Add project files.

---
+++ BadSyringe.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using RoR2;
+
+namespace PurpleLeprecaun
+{
+    [BepInDependency("com.bepis.r2api")]
+    //Change these
+    [BepInPlugin("com.ThatsSickLOL.BadSyringe", "Bad Syringe and if we see this exact name on Thunderstore we will deprecate your mod", "1.0.0")]
+    public class BadSyringe : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.EntityStates.Huntress.ArrowRain.OnEnter += (orig, self) =>
+            {
+
+                Chat.AddMessage("Hello");
+
+                orig(self);
+            };
+        }
+    }
+}
